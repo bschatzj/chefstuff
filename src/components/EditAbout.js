fddb17c34c6170b4ca4fe896ad9e93475e51f067
Andrew Maddocks <andrewmadd@icloud.com>
2019-12-28 19:46:39 -0700

    restyled the edit-about

--- src/components/EditAbout.js
+++ src/components/EditAbout.js
@@ -3,20 +3,24 @@ import React from "react";
 const EditAbout = () => {
   return (
     <div className="about-edit-div">
+      <div>
+        <h1>Edit About</h1>
+      </div>
       <form className="about-form">
-        <label className="about-label">About:</label>
+        {/* <label className="about-label"> About:</label> */}
         <textarea
-          rows="13"
+          rows="15"
           cols="70"
           style={{
             backgroundColor: "transparent",
             border: "2px solid #07fe20",
             fontSize: "18px",
-            color: "#f3ffef"
+            color: "#f3ffef",
+            width: "80%"
           }}
           type="text"
           name="about"
-          placeholder="Write here a little bit about you! Why you love to cook and why you want to share some of your awesome recipes!"
+          placeholder="Share a little bit about you here! Why you love to cook and why you want to share some of your awesome recipes!"
           className="about-input"
         />
 
