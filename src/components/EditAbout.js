e6a2fec58c99bd71b9a74736feb3ddcb2af349e2
Andrew Maddocks <andrewmadd@icloud.com>
2019-12-28 19:17:49 -0700

    worked on the edit-about component and finished styling

--- src/components/EditAbout.js
+++ src/components/EditAbout.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 const EditAbout = () => {
-  return <div></div>;
+  return (
+    <div className="about-edit-div">
+      <form className="about-form">
+        <label className="about-label">About:</label>
+        <textarea
+          rows="13"
+          cols="70"
+          style={{
+            backgroundColor: "transparent",
+            border: "2px solid #07fe20",
+            fontSize: "18px",
+            color: "#f3ffef"
+          }}
+          type="text"
+          name="about"
+          placeholder="Write here a little bit about you! Why you love to cook and why you want to share some of your awesome recipes!"
+          className="about-input"
+        />
+
+        <button className="about-button">Save Changes</button>
+      </form>
+    </div>
+  );
 };
 
 export default EditAbout;
